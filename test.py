fb8ddff873cf8b382f5385ab5ac0288a0650615a
Omer <omer.ornan@mail.huji.ac.il>
2016-03-05 13:48:07 +0200

    dns server-tcp check

--- test.py
+++ test.py
@@ -1,4 +1,36 @@
-# this is a test
+import  socket
+import dns
+import sys
+import dns.query
 
-print("or lets fuck");
-print("hhh")
+#
+# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# server_address = ('80.77.12.178', 10000)
+# sock.connect(server_address)
+# sock.send("h")
+# h=sock.recv((222));
+#
+IP_ADDRESS='60.6.75.31'
+queryCheck = dns.message.make_query('www.google.com', 2)
+dns.query.tcp(queryCheck,IP_ADDRESS)
+
+# def checkIfTcp(serverAddress):
+#   #  try:
+#         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#         server_address=(serverAddress,10000);
+#         sock.connect(server_address)
+#     #except:
+#         print("h")
+#
+# #checkIfTcp('www.google.co.il')
+#
+#
+#
+#
+# s =  socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+# result = s.connect_ex(('113.165.88.235 ', 10000))
+# s.close()
+# if result > 0:
+#     print("problem with socket!")
+# else:
+#     print("everything it's ok!")
