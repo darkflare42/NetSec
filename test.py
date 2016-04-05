e9a9087196eb3c8fa3269a5f909291f39a9c105a
Omer <omer.ornan@mail.huji.ac.il>
2016-04-05 11:02:59 +0300

    dns server-tcp check

--- test.py
+++ test.py
@@ -1,19 +1,36 @@
 import  socket
 import dns
-
 import dns.query
+import threading
 
-#
-# sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# server_address = ('80.77.12.178', 10000)
-# sock.connect(server_address)
-# sock.send("h")
-# h=sock.recv((222));sssd
-#
-IP_ADDRESS='60.6.75.31'
-queryCheck = dns.message.make_query('www.google.com', 2)
-dns.query.tcp(queryCheck,IP_ADDRESS)
 
+
+def createTcpConnection(threadName,IP_ADDRESS,SOURCE_PORT):
+    queryCheck = dns.message.make_query('www.google.com', 2)
+    while True:
+        print(threadName)
+        print(dns.query.tcp(queryCheck,IP_ADDRESS))
+
+# def portIsOpen(portNum):
+#     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+#     result = sock.connect_ex(('127.0.0.1', portNum))
+#     print(result)
+#     if result == 0:
+#         return True;
+#     return False
+
+
+def createThreads(NumberofThreads):
+    PORT_IN_USE = 1024
+    for i in range(NumberofThreads):
+        # while (portIsOpen(PORT_IN_USE) == False):
+        #     PORT_IN_USE += 1
+        PORT_IN_USE += 1
+        t=threading.Thread(target=createTcpConnection,args=("thread name %d"%(i,),'8.8.8.8',PORT_IN_USE))
+        t.start()
+
+
+createThreads(40)
 # def checkIfTcp(serverAddress):
 #   #  try:sdf
 #         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
