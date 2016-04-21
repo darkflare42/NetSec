ad5a5cb713e8c38341cde28125edfa01d844732d
Or Keren(Home) <or.keren1@mail.huji.ac.il>
2016-04-21 10:16:17 +0300

    Changed some stuff for HTTP Testing

--- readCSV.py
+++ readCSV.py
@@ -20,7 +20,7 @@ def startExhust(exhustServerInfo):
     print(exhustServerInfo)
     nsString=[]
     print(str(exhustServerInfo['ipv4']))
-    webInfo = test_HTTP_connection_tolerance(str(exhustServerInfo['ipv4']))
+    webInfo = test_HTTP_connection_tolerance(exhustServerInfo["DOM"], str(exhustServerInfo['ipv4']))
     wbSt='wsInfo: %d' % (webInfo,)
     print(wbSt)
 
@@ -46,7 +46,7 @@ def startExhust(exhustServerInfo):
 
 def main():
     # http_request("128.139.199.8")  # GOOGLE
-   # num_of_connections = test_HTTP_connection_tolerance("128.139.199.8")  # OR: This is to test the http tolerance
+    num_of_connections = test_HTTP_connection_tolerance("www.google.com" ,"128.139.199.8")  # OR: This is to test the http tolerance
     getURL=urlGenerator()
     url = next(getURL);
     writeToFile=csv.DictWriter(open('file3.csv','w'), delimiter=',',lineterminator='\n', fieldnames=headers)
